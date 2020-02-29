f2c52e39fabfde51f2241254bc550e2de44bda48
lxxjn0 <ljy3510@gmail.com>
2020-03-01 01:45:15 +0900

    feat : PostsResponseDto, PostsUpdateRequestDto 구현

--- src/main/java/com/lxxjn0/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/lxxjn0/book/springboot/service/posts/PostsService.java
@@ -4,8 +4,11 @@ import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import com.lxxjn0.book.springboot.domain.posts.Posts;
 import com.lxxjn0.book.springboot.domain.posts.PostsRepository;
+import com.lxxjn0.book.springboot.web.dto.PostsResponseDto;
 import com.lxxjn0.book.springboot.web.dto.PostsSaveRequestDto;
+import com.lxxjn0.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -17,6 +20,23 @@ public class PostsService {
 	@Transactional
 	public Long save(PostsSaveRequestDto requestDto) {
 		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+
+	@Transactional
+	public Long update(Long id, PostsUpdateRequestDto requestDto) {
+		Posts posts = postsRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+		posts.update(requestDto.getTitle(), requestDto.getContent());
+
+		return id;
+	}
+
+	public PostsResponseDto findById(Long id) {
+		Posts entity = postsRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+		return new PostsResponseDto(entity);
 	}
 
 }
