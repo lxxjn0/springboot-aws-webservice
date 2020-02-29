ea997e53abc6582d6de4470c8e9ad8cadbec7b3d
lxxjn0 <ljy3510@gmail.com>
2020-03-01 01:29:44 +0900

    feat : PostsApiController 테스트 코드 작성

--- src/main/java/com/lxxjn0/book/springboot/web/PostsApiController.java
+++ src/main/java/com/lxxjn0/book/springboot/web/PostsApiController.java
@@ -1,6 +1,6 @@
 package com.lxxjn0.book.springboot.web;
 
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +14,7 @@ public class PostsApiController {
 
 	private final PostsService postsService;
 
-	@PutMapping("/api/v1/posts")
+	@PostMapping("/api/v1/posts")
 	public Long save(@RequestBody PostsSaveRequestDto requestDto) {
 		return postsService.save(requestDto);
 	}
