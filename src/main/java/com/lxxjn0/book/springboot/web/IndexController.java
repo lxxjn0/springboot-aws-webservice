a2efe5e8a6b8bd63491f8afb142ffca67e8bdf94
lxxjn0 <ljy3510@gmail.com>
2020-03-02 22:08:06 +0900

    feat : posts-update.mustache 구현 및 게시글 수정을 위한 코드 수정

--- src/main/java/com/lxxjn0/book/springboot/web/IndexController.java
+++ src/main/java/com/lxxjn0/book/springboot/web/IndexController.java
@@ -3,8 +3,10 @@ package com.lxxjn0.book.springboot.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.lxxjn0.book.springboot.service.posts.PostsService;
+import com.lxxjn0.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -13,15 +15,23 @@ public class IndexController {
 
 	private final PostsService postsService;
 
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("posts", postsService.findAllDesc());
+		return "index";
+	}
+
 	@GetMapping("/posts/save")
 	public String postsSave() {
 		return "posts-save";
 	}
 
-	@GetMapping("/")
-	public String index(Model model) {
-		model.addAttribute("posts", postsService.findAllDesc());
-		return "index";
+	@GetMapping("/posts/update/{id}")
+	public String postsUpdate(@PathVariable Long id, Model model) {
+		PostsResponseDto dto = postsService.findById(id);
+		model.addAttribute("post", dto);
+
+		return "posts-update";
 	}
 
 }
