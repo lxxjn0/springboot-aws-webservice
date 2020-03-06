efe9e4cc98242a906aadb8124f678b0d97c11c65
lxxjn0 <ljy3510@gmail.com>
2020-03-06 19:07:22 +0900

    refactor : 전체 테스트 통과를 위한 코드 수정

--- src/main/java/com/lxxjn0/book/springboot/Application.java
+++ src/main/java/com/lxxjn0/book/springboot/Application.java
@@ -2,9 +2,7 @@ package com.lxxjn0.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication // 해당 Annotation이 있는 위치부터 설정을 읽어가기 때문에 항상 최상단에 위치해야 함
 public class Application { // 앞으로 만들 프로젝트의 메인 클래스
 
